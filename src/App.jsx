452e873141635a0c3524e2e419c459927aeebef9
Andrei Branza <andrei.branza@yahoo.com>
2024-05-17 15:16:25 +0300

    Added Log component and refactored code .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import Player from "./components/Player"
 import GameBoard from "./components/Gameboard"
+import Log from "./components/Log"
 import { useState } from 'react'
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([])
+  const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X')
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X'
+
+      if(prevTurns.length>0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O'
+      }
+
+      const updatedTurns = [{ square: {row: rowIndex, col: colIndex}, player: currentPlayer }, ...prevTurns]
+
+      return updatedTurns
+    })
   }
 
   return (
@@ -17,10 +30,13 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'}/>
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare} 
+          turns={gameTurns}
+        />
       </div>
 
-     LOG   
+     <Log turns={gameTurns}/>  
     </main>
   )
 }
